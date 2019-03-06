078ef78e92a786cadd82bed1acc7ddc95a63d1f7
tonytan4ever <tonytan198211@gmail.com>
2019-03-06 16:40:21 -0500

    Hack for django 1.11

--- billing/integration.py
+++ billing/integration.py
@@ -1,6 +1,11 @@
-from django.utils.importlib import import_module
+try:
+    # Django versions >= 1.9
+    from django.utils.module_loading import import_module
+except ImportError:
+    # Django versions < 1.9
+    from django.utils.importlib import import_module
+
 from django.conf import settings
-from django.conf.urls import patterns
 
 
 class IntegrationModuleNotFound(Exception):
@@ -47,7 +52,7 @@ class Integration(object):
 
     def get_urls(self):
         # Method must be subclassed
-        urlpatterns = patterns('')
+        urlpatterns = []
         return urlpatterns
 
     @property
